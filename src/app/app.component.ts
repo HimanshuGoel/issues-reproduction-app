374357de9bf9e063c68e0bd271777a3b679a7bfc
Himanshu Goel <himanshu.goel@soprabanking.com>
2022-12-14 12:10:10 +0530

    feat: changes to reproduce the import Jsep issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Jsep } from 'jsep';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'issues-reproduction-app';
+
+  ngOnInit() {
+    const parse_tree = Jsep.parse('1 + 1');
+    console.log(parse_tree);
+  }
 }
