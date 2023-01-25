cd347485f269ccdbbf4dcaf9bfd9c434580c7a0e
Himanshu Goel <himanshu.goel@soprabanking.com>
2023-01-25 15:47:39 +0530

    remove some unrelated dependencies and changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { AppComponent } from './app.component';
 import { ParentComponent } from './components/parent/parent.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatButtonModule } from '@angular/material/button';
 import { TestPageComponent } from './components/test-page/test-page.component';
 
 @NgModule({
@@ -17,8 +17,6 @@ import { TestPageComponent } from './components/test-page/test-page.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
-    MatInputModule,
-    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
